f500674b122a590bca70ad5c2d382059ba84d252
Edmond Hui <edmond.t.hui@gmail.com>
2020-11-28 13:17:31 -0500

    :tada: Initial commit, learning new technology MERN Stack

---
+++ mern-twitter/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const db = require(`./config/keys`).mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "Edmond",
+    email: "Edmond@edmond.com",
+    password: "password123",
+  });
+  user.save();
+  res.send("Hello World");
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Serve is running on port ${port}`));
